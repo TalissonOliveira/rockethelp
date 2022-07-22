1d819c04de8828ae92f1432bc3ff43870bedc7e4
Talisson Oliveira <talissonlux@outlook.com>
2022-07-21 21:11:13 -0300

    feat: creates the login screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,42 @@
-import { Heading, VStack } from 'native-base'
+import { useState } from 'react'
+import { Heading, Icon, useTheme, VStack } from 'native-base'
+import { Envelope, Key } from 'phosphor-react-native'
+import { Button } from '../components/Button'
+import { Input } from '../components/Input'
 
 import Logo from '../assets/logo_primary.svg'
 
 export function SignIn() {
+  const { colors } = useTheme()
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acessa sua conta
       </Heading>
+
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={<Icon as={
+          <Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setName}
+      />
+      <Input
+        mb={8}
+        placeholder="Senha"
+        secureTextEntry
+        InputLeftElement={<Icon as={
+          <Key color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setPassword}
+      />
+
+      <Button title="Entrar" w={"full"}/>
     </VStack>
   )
 }
