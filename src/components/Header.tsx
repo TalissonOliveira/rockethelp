8014a7d632e543f3d55a7d0c1b7394bbcd30b842
Talisson Oliveira <talissonlux@outlook.com>
2022-07-23 03:27:00 -0300

    feat: creates the header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { Heading, HStack, IconButton, StyledProps, useTheme } from 'native-base'
+import { CaretLeft } from 'phosphor-react-native'
+
+type HeaderProps = StyledProps & {
+  title: string
+}
+
+export function Header({ title, ...rest }: HeaderProps) {
+  const { colors } = useTheme()
+
+  return (
+    <HStack
+      {...rest}
+      w="full"
+      alignItems="center"
+      justifyContent="space-between"
+      bg="gray.600"
+      pb={6}
+      pt={12}
+    >
+      <IconButton
+        icon={<CaretLeft size={24} color={colors.gray[200]} />}
+      />
+
+      <Heading
+        color="gray.100"
+        textAlign="center"
+        fontSize="lg"
+        flex={1}
+        ml={-6}
+      >
+        {title}
+      </Heading>
+    </HStack>
+  )
+}
