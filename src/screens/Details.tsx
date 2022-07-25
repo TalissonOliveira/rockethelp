c60bdf58c989c1712cac63dca004d9fa646500a6
Talisson Oliveira <talissonlux@outlook.com>
2022-07-25 06:05:00 -0300

    feat: adds functionality to close the order

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,7 +1,8 @@
+import { Alert } from 'react-native'
 import { useEffect, useState } from 'react'
-import { useRoute } from '@react-navigation/native'
-import { HStack, ScrollView, Text, useTheme, VStack } from 'native-base'
-import { CircleWavyCheck, Clipboard, DesktopTower, Hourglass } from 'phosphor-react-native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { Box, HStack, ScrollView, Text, useTheme, VStack } from 'native-base'
+import { CircleWavyCheck, Clipboard, ClipboardText, DesktopTower, Hourglass } from 'phosphor-react-native'
 import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO'
 import { dateFormat } from '../utils/firestoreDateFormat'
 import { CardDetails } from '../components/CardDetails'
@@ -30,6 +31,31 @@ export function Details() {
   const route = useRoute()
   const { orderId } = route.params as RouteParams
 
+  const navigation = useNavigation()
+
+  function handleOrderClose() {
+    if (!solution) {
+      return Alert.alert('Solicitação', 'Informe a solução para finalizar a solicitação.')
+    }
+
+    firestore()
+      .collection<OrderFirestoreDTO>('orders')
+      .doc(orderId)
+      .update({
+        status: 'closed',
+        solution,
+        closed_at: firestore.FieldValue.serverTimestamp()
+      })
+      .then(() => {
+        Alert.alert('Solicitação', 'Solicitação finalizada.')
+        navigation.goBack()
+      })
+      .catch(error => {
+        console.log(error)
+        Alert.alert('Solicitação', 'Não foi possível finalizar a solicitação.')
+      })
+  }
+
   useEffect(() => {
     firestore()
       .collection<OrderFirestoreDTO>('orders')
@@ -54,14 +80,15 @@ export function Details() {
       })
   }, [])
 
-
   if (isLoading) {
     return <Loading />
   }
 
   return (
     <VStack flex={1} bg="gray.700">
-      <Header title="Solicitação" />
+      <Box bg="gray.600" px={6}>
+        <Header title="Solicitação" />
+      </Box>
 
       <HStack bg="gray.500" justifyContent="center" p={4}>
         {
@@ -86,33 +113,43 @@ export function Details() {
           title="Equipamento"
           icon={DesktopTower}
           description={`Patrimônio ${order.patrimony}`}
-          footer={order.when}
         />
 
         <CardDetails
           title="Descrição do problema"
-          icon={Clipboard}
+          icon={ClipboardText}
           description={order.description}
+          footer={`Registrado em ${order.when}`}
         />
 
         <CardDetails
           title="Solução"
           icon={CircleWavyCheck}
+          description={order.solution}
           footer={order.closed && `Encerrado em ${order.closed}`}
         >
-          <Input
-            placeholder="Descrição da solução"
-            onChangeText={setSolution}
-            textAlignVertical="top"
-            multiline
-            h={24}
-          />
+          {
+            order.status === 'open' && (
+              <Input
+                placeholder="Descrição da solução"
+                onChangeText={setSolution}
+                textAlignVertical="top"
+                multiline
+                h={48}
+            />
+            )
+          }
         </CardDetails>
       </ScrollView>
 
       {
         order.status === 'open' && (
-          <Button title="Finalizar" m={5} />
+          <Button
+            title="Finalizar"
+            m={5}
+            isLoading={isLoading}
+            onPress={handleOrderClose}
+          />
         )
       }
     </VStack>
